6e55334d64c0c8d9022d01a84695fe3798900988
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2025-01-04 11:23:58 +0530

    Components created

--- day_11/App.js
+++ day_11/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
+      <AboutUs />
+      <ContactUs />
       <StatusBar style="auto" />
     </View>
   );
---
+++ day_11/components/AboutUs.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { PaperProvider, Text } from 'react-native-paper';
+
+export default function AboutUs() {
+  return (
+          <PaperProvider>
+              <Text style={styles.header} variant="headlineLarge">AboutUs</Text>
+              <Text style={styles.body} variant="bodyLarge">
+                  Peter always saw the world in black and white. There were two choices for every situation and you had to choose one of them. It was therefore terribly uncomfortable for him to spend time with Ashley. She saw the world in shades of gray with hundreds of choices to choose from in every situation.
+              </Text>
+              <StatusBar style="auto" />
+          </PaperProvider>
+      )
+  }
+  
+  const styles = StyleSheet.create({
+      header: {
+          color: 'black',
+          textAlign: 'center',
+          padding: 10,
+      },
+      body: {
+          color: 'black',
+          textAlign: 'justify',
+          padding: 10,
+      },
+  });
+  
+  
