1158b8abc4f9de1d1dd3d76353c4a3d81d19412b
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2025-01-06 06:31:52 +0530

    AboutUs page finalized

--- day_11/components/AboutUs.js
+++ day_11/components/AboutUs.js
@@ -1,21 +1,29 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { PaperProvider, Text, Appbar } from 'react-native-paper';
+import { PaperProvider, Text, Card, Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 export default function AboutUs() {
+    const navigation = useNavigation();
     return (
         <PaperProvider>
-            <Appbar.Header>
-                <Appbar.Content title="About Us" />
-            </Appbar.Header>
-            
-            <View style={styles.container}>
-                <Text style={styles.title}>History</Text>
-                <Text style={styles.description}>
-                    The Vavuniya Campus of the University of Jaffna was established on 1st April 1997, upgraded from The Northern Province Affiliated University College, under an Order made under Section 22 of the Universities Act No. 16 of 1978, with two faculties: Business Studies and Applied Science.
-                </Text>
-            </View>
+            <Card>
+                <View style={styles.container}>
+                    <Text style={styles.title}>History</Text>
+                    <Text style={styles.description}>
+                        The Vavuniya Campus of the University of Jaffna was established on 1st April 1997, upgraded from The Northern Province Affiliated University College, under an Order made under Section 22 of the Universities Act No. 16 of 1978, with two faculties: Business Studies and Applied Science.
+                    </Text>
+                    <View style={styles.buttonContainer}>
+                        <Button mode="contained" onPress={() => navigation.navigate("Home")} style={styles.button}>
+                            Home
+                        </Button>
+                        <Button mode="contained" onPress={() => navigation.navigate("ContactUs")} style={styles.button}>
+                            Contact Us
+                        </Button>
+                    </View>
+                </View>
+            </Card>
 
             <StatusBar style="auto" />
         </PaperProvider>
@@ -42,5 +50,15 @@ const styles = StyleSheet.create({
         color: '#555',
         textAlign: 'justify',
         lineHeight: 24,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20,
+    },
+    button: {
+        flex: 1,
+        marginHorizontal: 5,
+        width: 150,
     },
 });
